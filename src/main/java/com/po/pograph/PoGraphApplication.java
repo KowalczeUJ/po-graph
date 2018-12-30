8dc8444796d6b9cecaa6cc018d31b7b702d546fa
kacper <kacper@crustlab.com>
2018-12-30 12:09:52 +0100

    Initial commit

---
+++ src/main/java/com/po/pograph/PoGraphApplication.java
@@ -0,0 +1,14 @@
+package com.po.pograph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoGraphApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/po/pograph/PoGraphApplicationTests.java
@@ -0,0 +1,17 @@
+package com.po.pograph;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoGraphApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
