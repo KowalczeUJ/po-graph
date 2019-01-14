182df9aaa1369a3e1a956eded351d3c55c13aae4
kacper <kacper@crustlab.com>
2019-01-14 13:31:35 +0100

    Add @RequestParam annotation to controller methods

--- src/main/java/com/po/pograph/controller/EdgeController.java
+++ src/main/java/com/po/pograph/controller/EdgeController.java
@@ -1,15 +1,14 @@
 package com.po.pograph.controller;
 
+import com.po.pograph.Service.GraphReadService;
 import com.po.pograph.graph.Graph;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.po.pograph.Service.GraphReadService;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -22,19 +21,14 @@ public class EdgeController {
 
     @ResponseBody
     @GetMapping(path = "add", produces = APPLICATION_JSON_VALUE)
-    public Graph addEdge() {
+    public Graph addEdge(@RequestParam("node1_id") int node1_id, @RequestParam("node2_id") int node2_id) {
         return graphReadService.getReadGraph();
     }
 
     @ResponseBody
     @DeleteMapping(path = "/delete", produces = APPLICATION_JSON_VALUE)
-    public Graph deleteEdge() {
+    public Graph deleteEdge(@RequestParam("node1_id") int node1_id, @RequestParam("node2_id") int node2_id) {
         return null;
     }
 
-    @ResponseBody
-    @PatchMapping(path = "change", produces = APPLICATION_JSON_VALUE)
-    public Graph changeEdgeValue() {
-        return null;
-    }
 }
--- src/main/java/com/po/pograph/controller/VertexController.java
+++ src/main/java/com/po/pograph/controller/VertexController.java
@@ -3,7 +3,9 @@ package com.po.pograph.controller;
 import com.po.pograph.graph.Graph;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +17,19 @@ public class VertexController {
 
     @ResponseBody
     @GetMapping(path = "/add", produces = APPLICATION_JSON_VALUE)
-    public Graph addVertex() {
+    public Graph addVertex(@RequestParam("node_id") int nodeId, @RequestParam("precursor_id") int precursorId) {
         return null;
     }
 
     @ResponseBody
     @DeleteMapping(path = "delete", produces = APPLICATION_JSON_VALUE)
-    public Graph deleteVertex() {
+    public Graph deleteVertex(@RequestParam("node_id") int nodeId) {
+        return null;
+    }
+
+    @ResponseBody
+    @PutMapping(path = "change", produces = APPLICATION_JSON_VALUE)
+    public Graph swapNodes(@RequestParam("node1_id") int firstNodeId, @RequestParam("node1_id") int secondNodeId) {
         return null;
     }
 
